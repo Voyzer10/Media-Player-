a29fda7869dd825621254211cca05e2f699794a1
garvit0404 <garvit142004@gmail.com>
2025-04-07 23:05:45 +0530

    first commit

---
+++ media-playeer/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-2xl font-bold  bg-black text-white'>
+       Hello World  
+      </div>
+      
+    </>
+  )
+}
+
+export default App
